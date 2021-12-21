701ebb5874634ed8dd3f0d9c2ae125a38b60236d
ThushanthSbm <56014745+ThushanthSbm@users.noreply.github.com>
2021-12-21 12:18:18 +0530

    student_create bootstrap

--- resources/views/student/create.blade.php
+++ resources/views/student/create.blade.php
@@ -15,22 +15,46 @@
   @endif -->
 
   <div class="container">
-  <h1>Student Registration Form</h1>
-<form action ="{{route('students.store')}}" method = "POST">
+  <div class="card card-primary"style="background-color:lightblue">
+
+  <div class="card-header">
+                <h3 class="card-title"> Student Registration Form</h3>
+                <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+              <i class="fas fa-minus"></i></button>
+            <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
+              <i class="fas fa-times"></i></button>
+          </div>
+              </div>
+
+ 
+<form action ="{{route('students.store')}}" method = "POST" enctype="multipart/form-data">
 
 @csrf
+<div class="card-body">
+               
+                <div class="form-group">
 <label for="fname">First Name</label>
-<input type ="text" name="fname" id="fname" placeholder="Type first name" value ="{{ old('fname')}}" class="@error('fname') is-invalid @enderror" ><br>
+<input type ="text" class="form-control" name="fname" id="fname" placeholder="Type first name" value ="{{ old('fname')}}" class="@error('fname') is-invalid @enderror" ><br>
   @error('fname')
-   <span style="color:red"> {{ $message }}</span>
-@enderror
-<br><br>
+  <div class="alert alert-danger">{{ $message }}</div>
+@enderror<div class="input-group-append">
+            
+          </div>
+
+               
+<div class="form-group">
 <label for="lname">Last Name</label>
-<input type ="text" name="lname" id="lname" placeholder="Type last name" value ="{{ old('lname')}}" class="@error('lname') is-invalid @enderror" ><br>
+<input type ="text" class="form-control" name="lname" id="lname" placeholder="Type last name" value ="{{ old('lname')}}" class="@error('lname') is-invalid @enderror" ><br>
   @error('lname')
-  <span style="color:red"> {{ $message }}</span>
-@enderror
-<br><br>
+  <div class="alert alert-danger">{{ $message }}</div>
+@enderror<div class="input-group-append">
+            
+          </div>
+
+
+
+          <div class="form-group">
 <label for ="gender">Gender:</label>
 @error('gender') is-invalid @enderror
 <input type="radio" id="male" name="gender" value="male" checked>
@@ -38,25 +62,50 @@
 <input type="radio" id="female" name="gender" value="female">
 <label for="female">Female</label>
 @error('gender')
-<span style="color:red"> {{ $message }}</span>
+<div class="alert alert-danger">{{ $message }}</div>
 @enderror
+                        </div> 
+                      </div>
 <br><br>
-<label for="grade">Select Grade</label>
 
-<select name="grade" id="grade">
+
+
+
+<div class="col-sm-12">
+                      <!-- select -->
+                      <div class="form-group">
+<label for="grade">Select Grade</label>
+<select name="grade" id="grade" class="form-control @error('grade') is-invalid @enderror" value ="{{ old('grade')}}">
   @foreach($grades as $grade)
   <option value="{{$grade->grade_name}}">{{$grade->grade_name}}</option>
   @endforeach
 </select>
+@error('grade')
+    <div class="alert alert-danger">{{ $message }}</div>
+@enderror</div>
+                      </div>
 <br><br>
+
+
+<div class="form-group">
 <label for="address">Address :</label>
-<textarea name="address" id="address" cols="30" rows="5"placeholder="type address" value ="{{ old('address')}}" class="@error('address') is-invalid @enderror" >
+<textarea name="address" class="form-control" id="address" cols="30" rows="5"placeholder="type address" value ="{{ old('address')}}" class="@error('address') is-invalid @enderror" >
 </textarea>
  @error('address')
-  <span style="color:red"> {{ $message }}</span>
-@enderror
+ <div class="alert alert-danger">{{ $message }}</div>
+@enderror<div class="input-group-append">
+            
+          </div>
+
 <br><br>
-<label for="subject">Subjects</label><br>
+
+
+         <div class="row">
+                    <div class="col-sm-6">
+                      <!-- checkbox -->
+                      <div class="form-group">
+                        <!-- <div class="form-check"> -->
+<label for="subject" class=" @error('subject') is-invalid @enderror">Subjects</label><br>
 @foreach($subjects as $subject)
 <input type="checkbox" id="subject" name="subject[]" value="{{$subject->subject_name}}"  >
 <label for="subject"> {{$subject->subject_name}}</label>
@@ -67,29 +116,60 @@
 <label for="Tamil"> Tamil</label>
 <input type="checkbox" id="English" name="subject[]" value="English">
 <label for="English"> English</label> -->
+@error('subject')
+    <div class="alert alert-danger">{{ $message }}</div>
+@enderror</div>
+                        </div>
+                        <!-- </div> -->
+</div>
 <br><br>
+
+
+<div class="form-group">
 <label for="birthday">Birthday:</label>
-<input type="date" id="birthday" name="birthday"  value ="{{ old('birthday')}}" class="@error('birthday') is-invalid @enderror" ><br>
+<input type="date" class="form-control" id="birthday" name="birthday"  value ="{{ old('birthday')}}" class="@error('birthday') is-invalid @enderror" ><br>
   @error('birthday')
-  <span style="color:red"> {{ $message }}</span>
-@enderror
+  <div class="alert alert-danger">{{ $message }}</div>
+@enderror<div class="input-group-append">
+            
+          </div>
 <br><br>
+
+
+<div class="form-group">
 <label for="email">Enter your email:</label>
-<input type="email" id="email" name="email" placeholder="Type email address" value ="{{ old('email')}}" class="@error('email') is-invalid @enderror" ><br>
+<input type="email" class="form-control" id="email" name="email" placeholder="Type email address" value ="{{ old('email')}}" class="@error('email') is-invalid @enderror" ><br>
   @error('email')
-  <span style="color:red"> {{ $message }}</span>
-@enderror
+  <div class="alert alert-danger">{{ $message }}</div>
+@enderror<div class="input-group-append">
+            
+          </div>
 <br><br>
+
+
+<div class="form-group">
 <label for="phone">Enter your phone number:</label>
-<input type="phone" id="phone" name="phone" placeholder="Type phone no" value ="{{ old('phone')}}" class="@error('phone') is-invalid @enderror" ><br>
+<input type="phone" class="form-control" id="phone" name="phone" placeholder="Type phone no" value ="{{ old('phone')}}" class="@error('phone') is-invalid @enderror" ><br>
   @error('phone')
-  <span style="color:red"> {{ $message }}</span>
-@enderror
+  <div class="alert alert-danger">{{ $message }}</div>
+@enderror<div class="input-group-append">
+            
+          </div>
 <br><br>
-<input type="submit" value="Save">
+
+
+<!-- <input type="submit" value="Save"> -->
+
+<div class="card-footer">
+                <a href="{{route('students.index')}}" class="btn btn-secondary btn-lg">Cancel</a>
+                  <button type="submit" class="btn btn-primary  btn-lg  " value="save">Submit</button>
+                </div>
+                </div>
+
 
 
 </form>
+</div>
 </div>
 </body>
 </html>
